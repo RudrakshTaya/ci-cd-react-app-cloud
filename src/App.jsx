faab861a1b7c6b6a033f609cd28d9e096e790dba
RudrakshTaya <rudrakshtaya8777.com>
2025-05-02 16:49:36 +0530

    github aws cloud

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+
+    <h1>Github</h1>
+    <h2>Rudraksh Taya</h2>
+    </div>
+      
+  )
+}
+
+export default App
